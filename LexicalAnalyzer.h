a58ea731eee4316e0e4bfc1fe21cb3b3c0cf60f1
AndyVera <42859114+AndyVera@users.noreply.github.com>
2021-12-07 22:52:16 -0800

    Final version without the Symbol Table

---
+++ LexicalAnalyzer.h
@@ -0,0 +1,299 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+
+#ifndef LexicalAnalyzer_h
+#define LexicalAnalyzer_h
+
+using namespace std;
+ifstream inputfile;
+ofstream outputfile;
+int current_state = 0;
+int previous_state = 0;
+string lexeme_string_source;
+string::iterator ptr;
+char lexeme_string_input;
+int index___ = 0;
+int line_number = 0;
+
+struct Lexeme {
+  string lexeme_word;
+  string lexeme_token;
+};
+
+void change_stream_contents (const char* input, const char* output){
+  inputfile.open(input);
+  outputfile.open(output);
+}
+
+int checking_proper_streams(){
+  if (inputfile.is_open() != 1){
+    cout << "Could not properly open the input file to fetch data from\n";
+    return 1;
+  }
+  if(outputfile.is_open() != 1){
+    cout << "Could not properly open the outut file to insert data into\n";
+    return 1;
+  }
+  return 0;
+}
+
+void checking_input_and_starting_iterator(){
+string lexeme_string_first ((istreambuf_iterator<char>(inputfile)), istreambuf_iterator<char>());
+lexeme_string_source = lexeme_string_first;
+ptr = lexeme_string_source.begin();
+
+//outputfile <<"----------Lexeme---------|---------Token---------\n";
+}
+
+string keywords[13] = {"function", "integer", "boolean", "real", "if", "endif", "else", "return", "put", "get", "while", "true", "false"} ;
+
+string separators[7] = { "}",		"{",		"(",		")",
+			",",	";", "#"};
+
+string operators[11] = {"<=",	 "+",		"-", 	"*",
+			"/",		 ">",		"<",		"=>",
+			"!=",	 "=", "==" };
+
+void closing_streams(){
+    inputfile.close();
+    outputfile.close();
+}
+
+Lexeme Lexer() {
+
+  int DFSM[5][4];
+  DFSM[0][0] = 0; DFSM[0][1] = 1; DFSM[0][2] = 2; DFSM[0][3] = 3;
+
+  DFSM[1][0] = 1; DFSM[1][1] = 1; DFSM[1][2] = 1; DFSM[1][3] = 3;
+
+  DFSM[2][0] = 2; DFSM[2][1] = 3; DFSM[2][2] = 2; DFSM[2][3] = 4;
+
+  DFSM[3][0] = 3; DFSM[3][1] = 3; DFSM[3][2] = 3; DFSM[3][3] = 3;
+
+  DFSM[4][0] = 4; DFSM[4][1] = 3; DFSM[4][2] = 4; DFSM[4][3] = 3;
+
+  string lexeme_string;
+
+  //struct object
+  Lexeme lexeme_output;
+
+  while(*ptr != '\0'){
+   lexeme_string_input = *ptr;
+
+    //State 0
+    if(isalpha(lexeme_string_input) && current_state == 0 ){
+        current_state = DFSM[current_state][1];
+        lexeme_string += lexeme_string_input;
+        //cout << "so far: " << lexeme_string << '\n';
+    }
+    else if(isdigit(lexeme_string_input) && current_state == 0 ){
+        current_state = DFSM[current_state][2];
+        lexeme_string += lexeme_string_input;
+        //cout << "so far: " << lexeme_string << '\n';
+    }
+    else if((lexeme_string_input == '.') && current_state == 0 ){
+        current_state = DFSM[current_state][3];
+    }
+
+    //State 1
+    else if (isalpha(lexeme_string_input) && current_state == 1){
+      //  previous_state = current_state;
+        current_state = DFSM[current_state][1];
+        lexeme_string += lexeme_string_input;
+        //cout << "so far: " << lexeme_string << '\n';
+    }
+    else if (lexeme_string_input == '_' && current_state == 1){
+      current_state = DFSM[current_state][1];
+      lexeme_string += lexeme_string_input;
+    }
+
+    else if (isdigit(lexeme_string_input) && current_state == 1){
+      //  previous_state = current_state;
+        current_state = DFSM[current_state][2];
+        lexeme_string += lexeme_string_input;
+        //cout << "so far: " << lexeme_string << '\n';
+
+    }
+
+    else if(lexeme_string_input == '.' && current_state == 1){
+      //  previous_state = current_state;
+        current_state = DFSM[current_state][3];
+        lexeme_string += lexeme_string_input;
+        //cout << "so far: " << lexeme_string << '\n';
+
+    }
+
+    //State 2
+    else if (isdigit(lexeme_string_input) && current_state == 2){
+        //previous_state = current_state;
+        current_state = DFSM[current_state][2];
+        lexeme_string += lexeme_string_input;
+        //cout << "so far: " << lexeme_string << '\n';
+    }
+
+    else if (isalpha(lexeme_string_input) && current_state == 2){
+        //cout << setw(16) << "Integer" << "|" << setw(17) << lexeme_string << '\n';
+        current_state = DFSM[current_state][1];
+        lexeme_string += lexeme_string_input;
+        //cout << "so far: " << lexeme_string << '\n';
+    }
+
+    else if(lexeme_string_input == '.' && current_state == 2){
+    //previous_state = current_state;
+    current_state = DFSM[current_state][3];
+    lexeme_string += lexeme_string_input;
+    //cout << "so far: " << lexeme_string << '\n';
+    }
+
+    // State 3
+    else if (isalpha(lexeme_string_input) && current_state == 3){
+      current_state = DFSM[current_state][1];
+      lexeme_string += lexeme_string_input;
+      //cout << "so far: " << lexeme_string << '\n';
+    }
+
+    else if (isdigit(lexeme_string_input) && current_state == 3){
+      current_state = DFSM[current_state][2];
+      lexeme_string += lexeme_string_input;
+      //cout << "so far: " << lexeme_string << '\n';
+    }
+
+    else if (lexeme_string_input == '.' && current_state == 3){
+    current_state = DFSM[current_state][3];
+    lexeme_string += lexeme_string_input;
+    //cout << "so far: " << lexeme_string << '\n';
+    }
+
+    //State 4
+
+    else if (isalpha(lexeme_string_input) && current_state == 4){
+      current_state = DFSM[current_state][1];
+      lexeme_string += lexeme_string_input;
+      //cout << "so far: " << lexeme_string << '\n';
+    }
+    else if (isdigit(lexeme_string_input) && current_state == 4){
+      current_state = DFSM[current_state][2];
+      lexeme_string += lexeme_string_input;
+      //cout << "so far: " << lexeme_string << '\n';
+    }
+    else if (lexeme_string_input == '.' && current_state == 4){
+      current_state = DFSM[current_state][3];
+      lexeme_string += lexeme_string_input;
+      //cout << "so far: " << lexeme_string << '\n';
+    }
+
+    //If its a white space, then stop lexer and identify lexeme
+    if(*ptr == ' ' || *ptr == '\n'){
+      if( *ptr == '\n'){
+        line_number += 1;
+      }
+      break;
+    }
+
+    //If it is not a white space, letter, digit, or decimal point, then it can be an
+    //operator or separator
+    if(*ptr != ' ' && *ptr != '\n' && *ptr != '.' && isdigit(lexeme_string_input) == 0 && isalpha(lexeme_string_input) == 0 ){
+      lexeme_string += lexeme_string_input;
+    }
+
+    ++ptr;
+
+  }
+
+
+
+    //checks if its a Operator or Separator
+    if(current_state == 0 ){
+      int sep_index, op_index;
+      for(int i = 0; i <= 6; ++i){
+        if( separators[i] == lexeme_string){
+          //outputfile << "\n----Lexeme: " << setw(16) << left << lexeme_string << "  Token: Separator----\n";
+          lexeme_output.lexeme_word = lexeme_string;
+          lexeme_output.lexeme_token = "Separator";
+          break;
+        }
+        sep_index = i + 1;
+      }
+      for(int j = 0; j <= 10; ++j){
+        if(operators[j] == lexeme_string){
+          //outputfile << "\n----Lexeme: " << setw(16) << left << lexeme_string << "  Token: Operator----\n";
+          lexeme_output.lexeme_word = lexeme_string;
+          lexeme_output.lexeme_token = "Operator";
+          break;
+        }
+        op_index = j + 1;
+      }
+      if (op_index == 11 && sep_index == 7){
+          //outputfile << "\n----Lexeme: " << setw(16) << left << lexeme_string << "  Token: Invalid----\n";
+          lexeme_output.lexeme_word = lexeme_string;
+          lexeme_output.lexeme_token = "Invalid";
+      }
+
+  }
+
+    //First checks if its a Keyword, if not then its an Identifier
+    if (current_state == 1){
+
+      for (int i = 0; i < 14; ++i){
+        if(keywords[i] == lexeme_string){
+          //outputfile << "\n----Lexeme: " << setw(16) << left << lexeme_string << "  Token: Keyword----\n";
+          lexeme_output.lexeme_word = lexeme_string;
+          lexeme_output.lexeme_token = "Keyword";
+          break;
+        }
+        index___ = i + 1;
+      }
+
+      if (index___ == 14){
+        //outputfile << "\n----Lexeme: " << setw(16) << left << lexeme_string << "  Token: Identifier----\n";
+        lexeme_output.lexeme_word = lexeme_string;
+        lexeme_output.lexeme_token = "Identifier";
+      }
+    }
+
+    //Checks if its an integer
+    if (current_state == 2){
+      //outputfile << "\n----Lexeme: " << setw(16) << left << lexeme_string << "  Token: Integer----\n";
+      lexeme_output.lexeme_word = lexeme_string;
+      lexeme_output.lexeme_token = "Integer";
+
+    }
+
+    //Checks if its an Invalid token
+    if (current_state == 3){
+      //outputfile << "\n----Lexeme: " << setw(16) << left << lexeme_string << "  Token: Invalid----\n";
+      lexeme_output.lexeme_word = lexeme_string;
+      lexeme_output.lexeme_token = "Invalid";
+    }
+
+    //Checks if its a Real
+    if(current_state == 4){
+      //outputfile << "\n----Lexeme: " << setw(16) <<  left <<lexeme_string << "  Token: Real----\n";
+      lexeme_output.lexeme_word = lexeme_string;
+      lexeme_output.lexeme_token = "Real";
+
+    }
+
+    //Resets values for next lexeme
+    current_state = 0;
+    ++ptr;
+
+    return lexeme_output;
+}
+
+
+
+#endif /*LexicalAnalyzer_h*/
+
+//for
+// Andy
+// ==
+// 000.000
+// Andy_Vera
+// Andy
+// 301
+// $
+// %
+// =>
---
+++ ObjectCodeGenerator.h
@@ -0,0 +1,576 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+#include "LexicalAnalyzer.h"
+
+#ifndef ObjectCodeGenerator_h
+#define ObjectCodeGenerator_h
+
+// All the variables needed
+Lexeme token1;
+Lexeme save;
+Lexeme backup;
+int addr;
+Lexeme relop;
+int index_num = 0;
+int sym_index_num = 0;
+int address_num = 7000;
+int flag1 = 0;
+int flag2 = 0;
+
+struct assembly_code {
+  int step_num;
+  string operation;
+  int address;
+};
+
+// struct symbol_entry {
+//   string iden;
+//   int sym_address;
+//   string data_type;
+// };
+
+int jumz_addr;
+
+assembly_code main_table[1000];
+//symbol_entry symbol_table[1000];
+assembly_code list_element;
+//symbol_entry symbol_index;
+
+// Declared all the functions needed corresponding to the Partial Solution given
+void simplified_Rat21F();//
+void Opt_Decleration_List_v2(); //
+void Declaration_list_v2(); //
+void Declaration_v2(); //
+void Qualifier_v2(); //
+void IDs_v2(); //
+void Statement_List_v2();//
+void Statement_v2();//
+void Compound_v2();//
+void Assign_v2();//
+void If_v2();//
+void Return_v2(); //
+void Print_v2();//
+void Scan_v2();//
+void While_v2();//
+void Condition_v2();//
+void Relop_v2();//
+void Expression_v2();//
+void Expression_prime_v2();//
+void Term_v2();//
+void Term_prime_v2();//
+void Factor_v2();//
+void print_assembly_table(assembly_code table[]);//
+//void print_symbol_table(symbol_entry table[]);
+
+//The Syntax Analyzer for the Simplified Rat21F language
+//with Object Code Generator
+void simplified_Rat21F(){
+
+  token1 = Lexer();
+
+  if(token1.lexeme_word == "#"){
+      token1 = Lexer();
+
+      Opt_Decleration_List_v2();
+
+      Statement_List_v2();
+
+      if(token1.lexeme_word == "#"){
+        print_assembly_table(main_table);
+        //print_symbol_table(symbol_table);
+      }
+      else {
+        cout << "What was expected was a '#'\n";
+      }
+  }
+  else{
+    cout << "What was expected was a '#'\n";
+  }
+
+}
+
+void Opt_Decleration_List_v2(){
+  Declaration_list_v2();
+}
+
+void Declaration_list_v2(){
+  Declaration_v2();
+
+  if(token1.lexeme_word == ";"){
+    token1 = Lexer();
+    if(token1.lexeme_word == "integer" || token1.lexeme_word == "boolean"){
+      //symbol_index.iden = token1.lexeme_word;
+      Declaration_list_v2();
+    }
+    else {
+      backup = token1;
+      flag1 = 1;
+    }
+  }
+  else{
+    cout << "What was expected was a ';'\n";
+  }
+}
+
+void Declaration_v2(){
+  Qualifier_v2();
+  IDs_v2();
+}
+
+void Qualifier_v2(){
+  if(token1.lexeme_word == "integer" || token1.lexeme_word == "boolean"){
+    //symbol_index.data_type = token1.lexeme_word;
+    token1 = Lexer();
+  }
+  else {
+    cout << "What was expected was 'integer' or boolean\n";
+  }
+}
+
+void IDs_v2(){
+   if (token1.lexeme_token == "Identifier"){
+  //   symbol_index.iden = token1.lexeme_word;
+  //   symbol_table[sym_index_num] = symbol_index;
+  //   sym_index_num++;
+    token1 = Lexer();
+    if(token1.lexeme_word == ","){
+      token1 = Lexer();
+      IDs_v2();
+    }
+  }
+  else {
+    cout << "What was expected was an Identifier\n";
+  }
+}
+
+void Statement_List_v2(){
+  Statement_v2();
+  if (token1.lexeme_word != "#"){
+  Statement_List_v2();
+}
+}
+
+void Statement_v2(){
+  if (flag1 == 0){
+    token1 = Lexer();
+  }
+  else if (flag1 == 1){
+    token1 = backup;
+    flag1 = 0;
+  }
+  Compound_v2();
+  Assign_v2();
+  While_v2();
+  If_v2();
+  Print_v2();
+  Scan_v2();
+  Return_v2();
+}
+
+void Compound_v2(){
+  if (token1.lexeme_word == "{"){
+    Statement_List_v2();
+    if (token1.lexeme_word == "}"){
+      ;
+    }
+    else {
+      cout << "What was expected was an '}'\n";
+    }
+  }
+}
+
+void Assign_v2(){
+    if (token1.lexeme_token == "Identifier"){
+
+      save = token1;
+      token1 = Lexer();
+
+      if(token1.lexeme_word == "="){
+        token1 = Lexer();
+
+        Expression_v2();
+
+        //instruction
+        list_element.step_num = index_num + 1 ;
+        list_element.operation = "POPM";
+        list_element.address = address_num;
+        main_table[index_num] = list_element;
+        index_num++;
+        address_num++;
+
+        if(token1.lexeme_word == ";"){
+          ;
+        }
+        else {
+          cout << "What was expected was a semicolon (;)\n";
+        }
+      }
+      else {
+        cout << "What was expected was an '='\n";
+      }
+    }
+}
+
+void Expression_v2(){
+  Term_v2();
+  Expression_prime_v2();
+}
+
+void Term_v2(){
+  Factor_v2();
+  Term_prime_v2();
+}
+
+void Factor_v2(){
+  if (token1.lexeme_token == "Identifier" || token1.lexeme_token == "Integer"){
+
+    //instruction
+    list_element.step_num = index_num + 1;
+    if(token1.lexeme_token == "Identifier"){
+      list_element.operation = "PUSHM";
+    }
+    else if (token1.lexeme_token == "Integer"){
+      list_element.operation = "PUSHI";
+    }
+    list_element.address = address_num;
+    main_table[index_num] = list_element;
+    index_num++;
+    address_num++;
+
+    token1 = Lexer();
+  }
+  else {
+    cout << "What was expected was an Identifier or an Integer\n";
+  }
+}
+
+void Term_prime_v2(){
+  Lexeme saved_temp;
+  if (token1.lexeme_word == "*" || token1.lexeme_word == "/"){
+    saved_temp = token1;
+    token1 = Lexer();
+    Factor_v2();
+
+    //instruction
+    list_element.step_num = index_num + 1 ;
+    if(saved_temp.lexeme_word == "*"){
+      list_element.operation = "MUL";
+    }
+    else if(saved_temp.lexeme_word == "/"){
+      list_element.operation = "DIV";
+    }
+    list_element.address = 0;
+    main_table[index_num] = list_element;
+    index_num++;
+
+    Term_prime_v2();
+  }
+}
+
+void Expression_prime_v2(){
+  Lexeme saved_temp;
+  if(token1.lexeme_word == "+" || token1.lexeme_word == "-"){
+    saved_temp = token1;
+    token1 = Lexer();
+    Term_v2();
+
+    //instruction
+    list_element.step_num = index_num + 1;
+    if(saved_temp.lexeme_word == "+"){
+      list_element.operation = "ADD";
+    }
+    else if(saved_temp.lexeme_word == "-"){
+      list_element.operation = "SUB";
+    }
+    list_element.address = 0;
+    main_table[index_num] = list_element;
+    index_num++;
+
+    Expression_prime_v2();
+
+  }
+}
+
+void While_v2(){
+  int saved_address;
+  if(token1.lexeme_word == "while"){
+    saved_address = index_num;
+    //instruction
+    list_element.step_num = index_num + 1;
+    jumz_addr = list_element.step_num;
+    list_element.operation = "LABEL";
+    list_element.address = 0;
+    main_table[index_num] = list_element;
+    index_num++;
+
+    token1 = Lexer();
+
+    if(token1.lexeme_word == "(") {
+      token1 = Lexer();
+
+      Condition_v2();
+      if(token1.lexeme_word == ")"){
+
+        Statement_v2();
+
+        list_element.step_num = index_num + 1;
+        list_element.operation = "JUMP";
+        list_element.address = saved_address;
+      }
+      else {
+        cout << "What was expected was a: ')'\n";
+      }
+    }
+    else {
+      cout << "What was expected was a '('\n";
+    }
+  }
+}
+
+void Condition_v2(){
+  Expression_v2();
+
+  if(token1.lexeme_token == "Operator"){
+    relop = token1;
+
+    token1 = Lexer();
+    Expression_v2();
+
+    //instructions{
+    if(relop.lexeme_word == "<"){
+      list_element.step_num = index_num + 1 ;
+      list_element.operation = "LES";
+      list_element.address = 0;
+      main_table[index_num] = list_element;
+      index_num++;
+
+      list_element.step_num = index_num + 1 ;
+      list_element.operation = "JUMPZ";
+      list_element.address = jumz_addr;
+      main_table[index_num] = list_element;
+      index_num++;
+    }
+    else if(relop.lexeme_word == ">"){
+      list_element.step_num = index_num + 1 ;
+      list_element.operation = "GRT";
+      list_element.address = 0;
+      main_table[index_num] = list_element;
+      index_num++;
+
+      list_element.step_num = index_num + 1 ;
+      list_element.operation = "JUMPZ";
+      list_element.address = jumz_addr;
+      main_table[index_num] = list_element;
+      index_num++;
+    }
+    else if(relop.lexeme_word == "=="){
+      list_element.step_num = index_num + 1 ;
+      list_element.operation = "EQU";
+      list_element.address = 0;
+      main_table[index_num] = list_element;
+      index_num++;
+
+      list_element.step_num = index_num + 1 ;
+      list_element.operation = "JUMPZ";
+      list_element.address = jumz_addr;
+      main_table[index_num] = list_element;
+      index_num++;
+    }
+    else if(relop.lexeme_word == "!="){
+      list_element.step_num = index_num + 1 ;
+      list_element.operation = "NEQ";
+      list_element.address = 0;
+      main_table[index_num] = list_element;
+      index_num++;
+
+      list_element.step_num = index_num + 1 ;
+      list_element.operation = "JUMPZ";
+      list_element.address = jumz_addr;
+      main_table[index_num] = list_element;
+      index_num++;
+    }
+    else if(relop.lexeme_word == "=>"){
+      list_element.step_num = index_num + 1 ;
+      list_element.operation = "GRT EQU";
+      list_element.address = 0;
+      main_table[index_num] = list_element;
+      index_num++;
+
+      list_element.step_num = index_num + 1 ;
+      list_element.operation = "JUMPZ";
+      list_element.address = jumz_addr;
+      main_table[index_num] = list_element;
+      index_num++;
+    }
+    else if(relop.lexeme_word == "<="){
+      list_element.step_num = index_num + 1 ;
+      list_element.operation = "LES EQU";
+      list_element.address = 0;
+      main_table[index_num] = list_element;
+      index_num++;
+
+      list_element.step_num = index_num + 1 ;
+      list_element.operation = "JUMPZ";
+      list_element.address = jumz_addr;
+      main_table[index_num] = list_element;
+      index_num++;
+    }
+  }
+  else {
+    cout << "What was expected was a Relational Operator\n";
+  }
+}
+
+void If_v2(){
+  if(token1.lexeme_word == "if"){
+    token1 = Lexer();
+
+      if(token1.lexeme_word == "("){
+        token1 = Lexer();
+        Condition_v2();
+
+        if (token1.lexeme_word == ")"){
+
+          Statement_v2();
+          //instruction
+          list_element.step_num = index_num + 1;
+          list_element.operation = "LABEL";
+          list_element.address = 0;
+          main_table[index_num] = list_element;
+          index_num++;
+
+
+          //cout << "The value of the lexeme is: " << token1.lexeme_word << "\n";
+          token1 = Lexer();
+          if (token1.lexeme_word == "endif"){
+            flag2 = 1;
+            token1 = Lexer();
+          }
+          else {
+            cout << "What was expected was 'endif'\n";
+          }
+        }
+        else {
+          cout << "What was expected was a ')'\n";
+        }
+      }
+      else {
+        cout << "What was expected was a '('\n";
+      }
+  }
+}
+
+void Print_v2(){
+  if (token1.lexeme_word == "put"){
+    token1 = Lexer();
+
+    if(token1.lexeme_word == "("){
+      token1 = Lexer();
+
+      Expression_v2();
+
+      if(token1.lexeme_word == ")"){
+        token1 = Lexer();
+
+        if(token1.lexeme_word == ";"){
+          ;
+        }
+        else {
+          cout << "What was expected was ';'\n";
+        }
+        list_element.step_num = index_num + 1;
+        list_element.operation = "STDOUT";
+        list_element.address = 0;
+        main_table[index_num] = list_element;
+        index_num++;
+      }
+      else {
+        cout << "What was expected was ')'\n";
+      }
+    }
+    else {
+      cout << "What was expected was '('\n";
+    }
+  }
+}
+
+void Scan_v2(){
+  if (token1.lexeme_word == "get"){
+    token1 = Lexer();
+
+    if (token1.lexeme_word == "("){
+      token1 = Lexer();
+
+      IDs_v2();
+
+      if(token1.lexeme_word == ")"){
+        token1 = Lexer();
+
+        if(token1.lexeme_word == ";"){
+          ;
+        }
+        else {
+          cout << "What was expected was ';'\n";
+        }
+        list_element.step_num = index_num + 1;
+        list_element.operation = "STDIN";
+        list_element.address = 0;
+        main_table[index_num] = list_element;
+        index_num++;
+      }
+      else {
+        cout << "What was expected was ')'\n";
+      }
+    }
+    else {
+      cout << "What was expected was '('\n";
+    }
+  }
+}
+
+void Return_v2(){
+  Lexeme saved_temp;
+  if(token1.lexeme_word == "return"){
+    saved_temp = token1;
+    token1 = Lexer();
+
+    Expression_v2();
+    if(token1.lexeme_word == ";"){
+      ;
+    }
+    list_element.step_num = index_num + 1;
+    list_element.operation = "STDOUT";
+    list_element.address = 0;
+    main_table[index_num] = list_element;
+    index_num++;
+
+  }
+}
+
+void print_assembly_table(assembly_code table[]) {
+
+  assembly_code temp;
+  int i = 0;
+  outputfile << "-----OBJECT GENERATED CODE-----\n";
+  while(table[i].step_num != 0){
+    temp = table[i];
+    outputfile << temp.step_num << " " << temp.operation << " " << temp.address << "\n";
+    i++;
+  }
+
+}
+
+// void print_symbol_table(symbol_entry table[]){
+//   symbol_entry temp2;
+//   int j = 0;
+//   outputfile << "\n----SYMBOL TABLE----\n";
+//   while(table[j].iden != "0"){
+//     temp2 = table[j];
+//     outputfile << temp2.iden << " " << temp2.sym_address << " " << temp2.data_type << "\n";
+//     j++;
+//   }
+// }
+
+#endif /*ObjectCodeGenerator_h*/
